66afcc0ad3979f190fb7e451868939f51e86223e
mdjack sparrow <mdjack01122000@gmail.com>
2019-11-28 21:42:43 -0800

    Deleted unneccessary files and add new work files

--- Nodes/myFirst.js
+++ Nodes/myFirst.js
@@ -1,8 +1,6 @@
 var http = require('http');
-var dt = require('./myFirstModule');
 
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/html'});
-  res.write("The date and time currently : " + dt.myDateTime());
   res.end('Hello World!');
 }).listen(8080);
