323f8d7a35d9a3679531648e7ce85e3c6487458e
mdjack sparrow <mdjack01122000@gmail.com>
2019-11-28 23:10:46 -0800

    learn new things 1.http_header 2.query_string 3.spliting_query_string 4.http_url

--- Nodes/myFirst.js
+++ Nodes/myFirst.js
@@ -1,9 +1,7 @@
-//create a variable to store http module
 var http = require('http');
 
-// create server Object 
 http.createServer(function (req,res) 
 {
   res.write('Hello world');
   res.end();
-}).listen(9524);
+}).listen(8080);
