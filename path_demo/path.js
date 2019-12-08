427689a0532733c24f2625c5cb00b4f8395afc09
mdjack sparrow <mdjack01122000@gmail.com>
2019-12-08 01:38:49 -0800

    Using fs module operations!

--- path_demo/path.js
+++ path_demo/path.js
@@ -1,3 +1,4 @@
+//for wrapper function confirmation
 // console.log(__filename);
 // console.log(__dirname);
 // console.log(module);
@@ -6,4 +7,61 @@
 
 var path = require('path');
 
-console.log(path.basename(__filename));
+//Base name
+// console.log(__filename);
+// console.log(path.basename('/path_demo/path.js'));
+
+//Delimiter
+// console.log(path.delimiter); //for windows ->  ; 
+// console.log(process.env.PATH.split(path.delimiter));
+
+//Dir-name
+// console.log(path.dirname(__filename));
+// console.log(path.basename(__filename));
+
+// Extension 
+// console.log(path.extname(path.basename(__filename)));
+
+// path format 
+// console.log(path.format({
+ // root:'C:\\',
+ // dir:'foo\\subFolder\\ass',
+ // base:'file.txt',
+ // name:'file',
+ // ext:'.txt'
+ // }));
+
+
+// Absolute path 
+// console.log(path.isAbsolute('C:\\path_demos//file'));
+
+// Join method 
+// console.log(path.join('\\foo','folder','file'));
+
+// Normalize method 
+// .. -> represent previous folder
+
+// console.log(path.normalize('\/\\/\/\/foo////folder///\/\/file'));
+
+
+// parse method 
+// console.log(path.parse("C:\\path_demo\\sass\\path.js").root);
+// console.log(path.parse("C:\\path_demo\\sass\\path.js").dir);
+// console.log(path.parse("C:\\path_demo\\sass\\path.js").base);
+// console.log(path.parse("C:\\path_demo\\sass\\path.js").ext);
+// console.log(path.parse("C:\\path_demo\\sass\\path.js").name);
+// console.log(path.parse("C:\\path_demo\\sass\\path.js"));
+
+// posix 
+// console.log(path.posix);
+
+// relative
+// console.log(path.relative("Folder\\sub\\filed","Folder\\sub\\file\\task.txt"));
+
+// Resolve method 
+// console.log(path.resolve('Foo','folder','file\\text'));
+// console.log(path.resolve("/folder","/file/pa_demo"));
+// console.log(path.resolve());
+
+
+console.log(path.resolve("test"));
